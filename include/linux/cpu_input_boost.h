dfa83f2b274fed3d3845e0f8572be2987e55e192
Dusan Uveric <dusan.uveric9@gmail.com>
2018-12-04 21:00:30 +0000

    Revert "SQUASH: cpu_input_boost: implement and hook general boosting"
    This reverts commit 933dfa18e573bf5ed801c11d9dae6e890b3b03ce.

--- include/linux/cpu_input_boost.h
+++ include/linux/cpu_input_boost.h
@@ -15,13 +15,9 @@
 
 #ifdef CONFIG_CPU_INPUT_BOOST
 void cpu_input_boost_kick(void);
-void cpu_general_boost_kick(unsigned int duration_ms);
 void cpu_input_boost_kick_max(unsigned int duration_ms);
 #else
 static inline void cpu_input_boost_kick(void)
-{
-}
-static inline void cpu_general_boost_kick(unsigned int duration_ms)
 {
 }
 static inline void cpu_input_boost_kick_max(unsigned int duration_ms)
